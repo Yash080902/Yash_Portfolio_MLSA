0ec0fb871296644d5f7fb8558c59cbce34a12b9d
Yash Kumar Goel <114879594+Yash080902@users.noreply.github.com>
2023-12-10 16:45:23 +0000

    some changes are done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /**
  * Home component
  *
@@ -13,16 +14,17 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "..images../campaign-creators-pypeCEaJeZY-unsplash.jpg";
+// import image from "..images../campaign-creators-pypeCEaJeZY-unsplash.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText =
+  "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
 const Home = ({ name, title }) => {
   return (
